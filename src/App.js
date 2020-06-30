be30f33c7b3a438fb746e47a8c3a1bc95c022051
Adrian Bratu <46399102+adirere@users.noreply.github.com>
2020-06-30 20:38:17 +0300

    added coronavirus icon and animated it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import React from "react";
-import styles from "./App.module.css";
 import Cards from "./components/Cards/Cards";
+import { makeStyles } from "@material-ui/core";
+import { ReactComponent as Logo } from "./images/coronavirus.svg";
+
+const useStyles = makeStyles({
+  container: {
+    textAlign: "center"
+  },
+  logo: {
+    position: "absolute",
+    width: "10%",
+    height: "15%",
+    top: "2%",
+    left: "1%",
+    animation: "example 5s linear 2s infinite alternate",
+    "@media (max-width:600px)": {
+      position: "relative",
+      margin: "1rem 0",
+      width: "20%"
+    },
+    "@media (max-width:300px)": {
+      position: "relative",
+      margin: "1rem 0",
+      width: "30%",
+      height: "10%"
+    }
+  }
+});
 
 export default function App() {
+  const classes = useStyles();
+
   return (
-    <div className={styles.container}>
+    <div className={classes.container}>
+      <Logo className={classes.logo} />
       <Cards />
     </div>
   );
