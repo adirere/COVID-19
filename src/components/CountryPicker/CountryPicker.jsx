e79c7d205e900b7ba709c0e95ffeb0a33aa0be5c
Adrian Bratu <46399102+adirere@users.noreply.github.com>
2020-07-05 22:33:45 +0300

    changed countryPicker component to use NativeSelect

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,51 +1,49 @@
 import React, { useState, useEffect } from "react";
 import {
-  Select,
-  MenuItem,
-  InputLabel,
   FormControl,
+  NativeSelect,
+  InputLabel,
   makeStyles
 } from "@material-ui/core";
 import { fetchCountries } from "../../api/index";
 
 const useStyles = makeStyles({
-  formControl: {
-    minWidth: 120
+  form: {
+    marginTop: "1rem"
+  },
+  customSelect: {
+    backgroundColor: "rgba(182,170,254,0.4)",
+    fontFamily: "Arial"
   }
 });
 
 const CountryPicker = ({ handleCountryChange }) => {
   const classes = useStyles();
-  const [selectedCountry, setSelectedCountry] = useState("");
   const [countriesPicker, setCountriesPicker] = useState([]);
 
   useEffect(() => {
     const fetchedCountries = async () => {
-      const countries = await fetchCountries();
-      setCountriesPicker(countries);
+      setCountriesPicker(await fetchCountries());
     };
     fetchedCountries();
   }, []);
 
-  const handleChange = e => {
-    setSelectedCountry(e.target.value);
-    handleCountryChange(e.target.value);
-  };
-
   return (
-    <div style={{ marginTop: "1rem" }}>
-      <FormControl className={classes.formControl}>
-        <InputLabel shrink>Select Country</InputLabel>
-        <Select value={selectedCountry} onChange={handleChange} autoWidth>
-          {countriesPicker &&
-            countriesPicker.map(country => (
-              <MenuItem key={country.iso3} value={country.name}>
-                {country.name}
-              </MenuItem>
-            ))}
-        </Select>
-      </FormControl>
-    </div>
+    <FormControl className={classes.form}>
+      <InputLabel shrink>Select a country</InputLabel>
+      <NativeSelect
+        className={classes.customSelect}
+        defaultValue=""
+        onChange={e => handleCountryChange(e.target.value)}
+      >
+        <option value="">Global</option>
+        {countriesPicker.map((country, i) => (
+          <option key={i} value={country.name}>
+            {country.name}
+          </option>
+        ))}
+      </NativeSelect>
+    </FormControl>
   );
 };
 
